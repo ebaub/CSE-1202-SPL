a71cd7bf2e701ad0136bd12f83b97c48bb1e7b1b
ebaub <mosiur.inbox@gmail.com>
2026-07-26 14:17:02 +0600

    Completed  calculators

--- Lab 01 - Operators/calculator.c
+++ Lab 01 - Operators/calculator.c
@@ -3,12 +3,14 @@
 int main()
 {
     printf("Calculator\n");
-    int x,y;
-    x = 10, y = 5;
-    printf("%d + %d = %d\n",x,y,x+y);
-    printf("%d - %d = %d\n",x,y,x-y);
-    printf("%d * %d = %d\n",x,y,x*y);
-    printf("%d / %d = %d\n",x,y,x/y);
-    printf("%d %% %d = %d\n",x,y,x%y);
+    double x,y;
+    printf("Enter two numbers: ");
+    scanf("%lf%lf",&x,&y);
+    
+    printf("%0.2lf + %0.2lf = %0.2lf\n",x,y,x+y);
+    printf("%0.2lf - %0.2lf = %0.2lf\n",x,y,x-y);
+    printf("%0.2lf * %0.2lf = %0.2lf\n",x,y,x*y);
+    printf("%0.2lf / %0.2lf = %0.2lf\n",x,y,x/y);
+    printf("%0.2lf %% %0.2lf = %d\n",x,y,(int)x % (int)y); // type casting
     return 0;
 }
