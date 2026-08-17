c6c87fd1a645073f9a6d45d3cbe35f74639a1a1c
ebaub <mosiur.inbox@gmail.com>
2026-08-17 14:19:40 +0600

    updated

--- Lab 01 - Operators/dollar_conversion_B.C
+++ Lab 01 - Operators/dollar_conversion_B.C
@@ -2,13 +2,12 @@
 
 int main()
 {
-    int y;
-    { // 404
-        int x = 5;
-        y=7;
-    }
-    
-        printf("%d\n", x);
+    double BDT, USD;
+    printf("Enter value in BDT: ");
+    scanf("%lf", &BDT);
+    USD = (int)(BDT / 77.5); // type casting
+    BDT = BDT - (USD * 77.5);
+    printf("%0.0lf USD and %0.2lf BDT\n", USD, BDT);
 
     return 0;
 }
