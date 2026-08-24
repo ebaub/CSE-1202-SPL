193c1369898377e5d8c4241e49640d79f039f112
ebaub <mosiur.inbox@gmail.com>
2026-08-24 13:41:52 +0600

    Updated path

---
+++ Lab 03 - Conditions (Switch)/tempCodeRunnerFile.c
@@ -0,0 +1,38 @@
+/*
+Calculate according to choice>:
+Choice Result
+1 Add
+2 Subtract
+3 Multiply
+4 Divide
+5 Remainder
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    double x,y;
+    int operator; // + - * / %
+    scanf("%lf %lf %d",&x,&y,&operator);
+    switch(operator) {
+        case 1:
+            printf("%0.2lf + %0.2lf =  %0.2lf\n",x,y,x+y);
+            // break;
+        case 2:
+            printf("%0.2lf - %0.2lf =  %0.2lf\n",x,y,x-y);
+            // break;
+        case 3:
+            printf("%0.2lf * %0.2lf =  %0.2lf\n",x,y,x*y);
+            // break;
+        case 4:
+            printf("%0.2lf / %0.2lf =  %0.2lf\n",x,y,x/y);
+            // break;
+        case 5:
+            printf("%0.2lf %% %0.2lf =  %d\n",x,y,(int)x%(int)y);
+            // break;
+        default:
+            printf("Invalid input\n");
+    }
+    return 0;
+}
